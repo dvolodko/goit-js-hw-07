4a902c7b007f0c788c3ac6d5565bd80f79441f92
Denys Volodko <babyleejane@gmail.com>
2022-11-22 21:03:02 +0200

    making gallery in proccess

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", galleryImageOpen);
+
+function createGalleryItemMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+		})
+		.join("");
+}
+
+function galleryImageOpen(e) {
+	e.preventDefault();
+	const isGalleryItem = e.target.classList.contains("gallery__image");
+	if (!isGalleryItem) {
+		return;
+	}
+
+	const selectedImage = e.target.dataset.source;
+	const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`);
+
+	instance.show();
+
+	window.addEventListener("keydown", e => {
+		if (e.code !== "Escape") {
+			return;
+		}
+
+		instance.close();
+	});
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", galleryImageOpen);
+
+function createGalleryItemMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+      <a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+      </a>`;
+		})
+		.join("");
+}
