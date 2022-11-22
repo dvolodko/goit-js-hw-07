70427251d876bdac814ab987896b57f69df1c290
Denys Volodko <babyleejane@gmail.com>
2022-11-22 23:44:21 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,16 +4,9 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const galleryContainer = document.querySelector(".gallery");
-const galleryItem = galleryContainer.querySelector(".gallery__item");
 const galleryMarkup = createGalleryItemMarkup(galleryItems);
-const lightbox = new SimpleLightbox(".gallery a", {
-	captionsData: "alt",
-	captionDelay: 250,
-});
 
 galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
-
-galleryContainer.addEventListener("click", galleryImageOpen);
 
 function createGalleryItemMarkup(galleryItems) {
 	return galleryItems
@@ -26,13 +19,7 @@ function createGalleryItemMarkup(galleryItems) {
 		.join("");
 }
 
-function galleryImageOpen(e) {
-	e.preventDefault();
-	const isGalleryItem = e.target.classList.contains("gallery__image");
-	if (!isGalleryItem) {
-		return;
-	}
-
-	console.log(lightbox);
-	lightbox.open();
-}
+new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
